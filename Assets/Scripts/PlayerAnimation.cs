14b440d13c02a1e5a07f52aac2224f9b95e2c514
Pedro Denardi Minuzzi <71407814+pedrindenard@users.noreply.github.com>
2023-05-19 23:40:05 -0300

    Palete Tiles and player interaction
    Creation of the game's initial map through the Tiles Palette, available from the 2017 version of Unity. Animations were also created with the assets, for example, torches. Finally, some character fixes and addition of interaction to objects. There was also a change in the physics of the player, now there is no more friction with the scenario.

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : MonoBehaviour {
-    
+public class PlayerAnimation : MonoBehaviour
+{
+
     private Animator playerAnimator;
     private Rigidbody2D playerRigidBody;
 
     public Collider2D playerStandardsCollider; // Player standards and crouch collider
     public Collider2D playerCrouchCollider; // Player crouch collide
 
+    public Transform playerInteraction; // Player interaction
     public Transform groundCheck; // Object responsible for detecting if the character is on a surface
+
+    public LayerMask groundLayerMask; // Layer mask for the character that is colliding with the ground
+    public LayerMask interactionLayerMask; // Layer mask for the character that is colliding with the interactive object
 
     public float playerSpeed; // Character movement speed
     public float playerJumpForce; // Force applied to generate the character's jump
 
-    public PlayerLookingState playerLookingState; // Indicates which direction the character is facing
+    public Vector3 playerDirection = Vector3.right; // Indicates which direction the character is facing
+    public PlayerLookingState playerLookingState; // Indicates which direction the character is looking
     public bool playerAttacking; // Indicates whether the character is performing an attack
     public bool playerInGround; // Indicates whether the character is stepping on any surface
 
@@ -25,19 +31,23 @@ public class PlayerAnimation : MonoBehaviour {
     public int idAnimation; // Indicates which animation is running
 
     // Initial call from script
-    void Start() {
+    void Start()
+    {
         playerAnimator = GetComponent<Animator>();
         playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Called every 0.02 seconds (Player physics simulation)
-    void FixedUpdate() {
-        playerRigidBody.velocity = new Vector2(horizontal * playerSpeed, playerRigidBody.velocity.y);
-        playerInGround = Physics2D.OverlapCircle(groundCheck.position, 0.02F);
+    void FixedUpdate()
+    {
+        playerVelocity();
+        playerPhysics();
+        playerInteract();
     }
 
     // Called every frame
-    void Update() {
+    void Update()
+    {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
@@ -51,91 +61,184 @@ public class PlayerAnimation : MonoBehaviour {
         playerAnimations();
     }
 
-    void playerMovingStates() {
-        if (vertical < 0) {
+    // Called every time the player collides with another collider
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Trunk":
 
+                break;
+            case "Thorns":
+
+                break;
+            case "Plate":
+
+                break;
+            case "Lever":
+
+                break;
+            case "Door":
+
+                break;
+            case "Steps":
+
+                break;
+        }
+    }
+
+    // Called every time the player left from collides
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Trunk":
+
+                break;
+            case "Thorns":
+
+                break;
+            case "Plate":
+
+                break;
+            case "Lever":
+
+                break;
+            case "Door":
+
+                break;
+            case "Steps":
+
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+        
+    }
+
+    void playerMovingStates()
+    {
+        if (vertical < 0)
+        {
             // Player is crouching
             idAnimation = 2;
-
             // If player is crouching, cannot move. But if player is grounded, can move
             if (groundCheck) horizontal = 0;
-
-        } else if (horizontal != 0) {
-
+        }
+        else if (horizontal != 0)
+        {
             // Player is walking
             idAnimation = 1;
-
-        } else {
-
+        }
+        else
+        {
             // Player is idle
             idAnimation = 0;
-
         }
     }
 
-    void playerLookingStates() {
-        if (horizontal > 0 && playerLookingState == PlayerLookingState.LEFT && !playerAttacking) {
+    void playerLookingStates()
+    {
+        if (horizontal > 0 && playerLookingState == PlayerLookingState.LEFT && !playerAttacking)
+        {
             flipPlayer();
-        } else if (horizontal < 0 && playerLookingState == PlayerLookingState.RIGHT && !playerAttacking) {
+        }
+        else if (horizontal < 0 && playerLookingState == PlayerLookingState.RIGHT && !playerAttacking)
+        {
             flipPlayer();
         }
     }
 
-    void playerCollider() {
+    void playerCollider()
+    {
         playerStandardsCollider.enabled = (vertical >= 0 && playerInGround) || ((vertical != 0 || vertical >= 0) && !playerInGround);
         playerCrouchCollider.enabled = vertical < 0 && playerInGround;
     }
 
-    void playerAnimations() {
+    void playerAnimations()
+    {
         playerAnimator.SetBool("Grounded", playerInGround);
         playerAnimator.SetInteger("IdAnimation", idAnimation);
         playerAnimator.SetFloat("SpeedY", playerRigidBody.velocity.y);
     }
 
-    void attackAnimation() {
-        if (Input.GetButtonDown("Fire1") && vertical >= 0 && !playerAttacking) {
+    void attackAnimation()
+    {
+        if (Input.GetButtonDown("Fire1") && vertical >= 0 && !playerAttacking)
+        {
             playerAnimator.SetTrigger("Attack");
         }
         // If player is running attack animation, stop moving
-        if (playerAttacking && playerInGround) {
+        if (playerAttacking && playerInGround)
+        {
             horizontal = 0;
         }
     }
 
-    void jumpAnimation() {
-        if (Input.GetButtonDown("Jump") && playerInGround && !playerAttacking) {
+    void jumpAnimation()
+    {
+        if (Input.GetButtonDown("Jump") && playerInGround && !playerAttacking)
+        {
             Vector2 jumpVector = new Vector2(0, playerJumpForce);
             playerRigidBody.AddForce(jumpVector);
         }
     }
 
-    void flipPlayer() {
-
-        switch (playerLookingState) {
-
+    void flipPlayer()
+    {
+        switch (playerLookingState)
+        {
             case PlayerLookingState.LEFT:
                 playerLookingState = PlayerLookingState.RIGHT;
                 break;
-
             case PlayerLookingState.RIGHT:
                 playerLookingState = PlayerLookingState.LEFT;
                 break;
-
             default:
                 playerLookingState = PlayerLookingState.LEFT;
                 break;
         }
 
         float scaleX = transform.localScale.x;
-        scaleX *= -1; // Inverter o valor do "scaleX"
-
+        scaleX *= -1; // Invert "scaleX" value
+        
         Vector3 scaleSettings = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
-        transform.localScale = scaleSettings;
+
+        transform.localScale = scaleSettings; // Set player looking direction
+        playerDirection.x = scaleX; // Set player Raycast direction
     }
 
-    public void playerAttackStates(int isAttacking) {
+    public void playerAttackStates(int isAttacking)
+    {
         // For some reason Unity doesn't understand boolean values in animations functions, so we need to check int values instead
         playerAttacking = isAttacking != 0;
+    }
+
+    void playerVelocity()
+    {
+        playerRigidBody.velocity = new Vector2(horizontal * playerSpeed, playerRigidBody.velocity.y);
+    }
+
+    void playerPhysics()
+    {
+        playerInGround = Physics2D.OverlapCircle(groundCheck.position, 0.02F, groundLayerMask);
+    }
+
+    void playerInteract()
+    {
+        // Create invisible line ray for player interactions
+        RaycastHit2D hit = Physics2D.Raycast(playerInteraction.position, playerDirection, 0.15F, interactionLayerMask);
+        Debug.DrawRay(playerInteraction.position, playerDirection * 0.15F, Color.red);
+
+        if (hit)
+        {
+            print(hit.collider.gameObject.tag);
+        }
     }
 
 }
